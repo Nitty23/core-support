b79fcfb195d9e9e70400389f154f9b885cf9d21f
gspeach <georgebspeach@outlook.com>
2019-02-21 12:17:39 -0500

    config setup

---
+++ Gruntfile.js
@@ -0,0 +1,100 @@
+module.exports = function(grunt) {
+
+    var configs = '_js/globals/configs/production.js',
+        i = 0,
+        len = grunt.cli.tasks.length;
+
+    //SET GLOBAL CONFIGS BASED ON ENVIROMENT OR TASK
+    for (i; i < len; i++) {
+        if (grunt.cli.tasks[i] === 'dev') {
+            configs = '_js/globals/configs/development.js';
+        }
+    }
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        critical: {
+            test: {
+                options: {
+                    inline: true,
+                    minify: true,
+                    base: './',
+                    css: [
+                        '_site/e2/css/rv7/core-support/style.css'
+                    ],
+                    dimensions: [{
+                        height: 768,
+                        width: 1366
+                    }, {
+                        height: 640,
+                        width: 360
+                    }, {
+                        height: 568,
+                        width: 320
+                    }]
+                },
+                src: './_site/index.html',
+                dest: './_site/index.html'
+            }
+        },
+        browserify: {
+            acft: {
+                files: {
+                    '_js/bundled/core-support.js': [configs, '_js/core-support.js']
+                },
+                options: {
+                    transform: ['babelify']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: true,
+                compress: true,
+                beautify: false
+            },
+            build: {
+                files: [{
+                    src: [
+                        '_js/bundled/core-support.js'
+                    ],
+                    dest: 'e2/js/rv7/core-support/<%= pkg.name %>.min.js'
+                }]
+            }
+        },
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %> */\n'
+                },
+                files: {
+                    src: ['_site/e2/js/**/*.js', '_site/e2/css/**/*.css']
+                }
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'e2/css/rv7/core-support/style.css': '_scss/_style.scss'
+                }
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-critical');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-banner');
+
+    grunt.registerTask('production', ['browserify', 'uglify', 'sass']);
+
+    grunt.registerTask('post-production', ['usebanner', 'critical']);
+
+};
---
+++ _js/core-support.js
@@ -0,0 +1,11 @@
+/*global document, require*/
+
+var ParallaxImg = require('./globals/modules/ParallaxImage'),
+    Helper = require('./globals/modules/Helper');
+
+(function () {
+    'use strict';
+
+    var test = "";
+        
+}());
